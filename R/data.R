669fb71ac43621e04804f90778fb982d96a56834
JCSzamosi <jc.szamosi@alumni.utoronto.ca>
2023-02-15 11:31:52 -0500

    start documenting the colour vector datasets

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' 21 Colours
+#'
+#' Used mostly for taxa bar charts
+#'
+#' @format ## `cols_21`
+#' A vector with 21 colour hex codes, used mostly in plotting taxa bar charts
+#' with 21 or fewer taxa.
+#'
+
+
+#' 31 Colours
+#'
+#' Used mostly for taxa bar charts
+#'
+#' @format ## `cols_21`
+#' A vector with 21 colour hex codes, used mostly in plotting taxa bar charts
+#' with 21 or fewer taxa.
